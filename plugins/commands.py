9034abdbbdf152fa67f1de5448a2703ff86975c4
ligermohit07 <mohittatu2007@gmail.com>
2025-04-24 12:21:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -175,7 +175,7 @@ async def start(client:Client, message):
          #       newPoint = await db.get_point(refUserId)
              if AUTH_CHANNEL and await is_req_subscribed(client, message):
                         buttons = [[
-                            InlineKeyboardButton('☆ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ☆', url=f'http://t.me/{temp.U_NAME}?startgroup=start')
+                            InlineKeyboardButton('☘️Bot Update Channel🍀', url='https://t.me/icrunch_update')
                         ],[
                             InlineKeyboardButton("⚙️Hᴇʟᴘ", callback_data='features'),
                             InlineKeyboardButton('Aʙᴏᴜᴛ💌', callback_data=f'about')
@@ -201,7 +201,7 @@ async def start(client:Client, message):
             pass
     if len(message.command) != 2:
         buttons = [[
-                            InlineKeyboardButton('☆ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ☆', url=f'http://t.me/{temp.U_NAME}?startgroup=start')
+                            InlineKeyboardButton('☘️Bots Update Channel🍀', url='https://t.me/icrunch_updates')
                         ],[
                             InlineKeyboardButton("⚙️Hᴇʟᴘ", callback_data='features'),
                             InlineKeyboardButton('Aʙᴏᴜᴛ💌', callback_data=f'about')
@@ -260,7 +260,7 @@ async def start(client:Client, message):
 
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('☆ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ☆', url=f'http://t.me/{temp.U_NAME}?startgroup=start')
+            InlineKeyboardButton('☘️Bots Update Channel🍀', url='https://t.me/icrunch_updates')
                         ],[
                             InlineKeyboardButton("⚙️Hᴇʟᴘ", callback_data='features'),
                             InlineKeyboardButton('Aʙᴏᴜᴛ💌', callback_data=f'about')
